6b9608ade186edc133f60c41172e86c4e7d8f3e5
kingJTao <951086498@qq.com>
2018-09-20 17:55:08 +0800

    算法修改

--- AVLtree.c
+++ AVLtree.c
@@ -21,5 +21,39 @@ void LeftBalance(BSTree *p){
 void RightBalance(BSTree *p){
 }
 
-int InsertAVL(BSTree *t,int e,int *taller){
+int InsertAVL(BSTree *t,BSTree parent,int e,int *taller){
+	int tall = 0;
+
+	if( !(*t) ){//空结点
+		*t = (BSTree)malloc(sizeof(BSTNode));
+		if( *t ){
+			(*t)->data = e;
+			(*t)->bf = EH;
+			(*t)->lchild = (*t)->rchild = NULL;
+			*taller = 1;
+			return 1;
+		}
+	}else{
+		if( EQ(e,(*t)->data) ){
+			return 0;
+		}else if( LT(e,(*t)->data) ){
+			if( !InsertAVL(&((*t)->lchild),*t,e,&tall) ){//插入结点失败
+				*taller = 0;
+				return 0;
+			}else{//插入结点成功
+				if( tall ){//树增高
+
+				}
+			}
+		}else{
+			if( !InsertAVL(&((*t)->rchild),*t,e,*tall) ){
+				*taller = 0;
+				return 0;
+			}else{
+				if( tall ){
+
+				}
+			}
+		}
+	}
 }
